939c59b300b546208379e1e2ef4acc3770e20fc8
DmIrina <dmytriieva.iryna@gmail.com>
2022-05-06 21:06:00 -0700

    Intersection of Two Arrays II
    Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.

---
+++ src/task11/Solution.java
@@ -0,0 +1,37 @@
+package task11;
+
+import java.util.Arrays;
+import java.util.Hashtable;
+
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Hashtable<Integer, Integer> hashtable = new Hashtable<>();
+        for (int item : nums1) {
+            if (hashtable.containsKey(item)) {
+                hashtable.replace(item, hashtable.get(item) + 1);
+            } else {
+                hashtable.put(item, 1);
+            }
+        }
+        int[] arr1 = new int[Math.min(nums1.length, nums2.length)];
+        int counter = 0;
+
+        for (int item: nums2) {
+            if (hashtable.containsKey(item) && hashtable.get(item) != 0){
+                hashtable.replace(item, hashtable.get(item) - 1);
+                arr1[counter] = item;
+                counter++;
+            }
+        }
+
+        int[] arr2 = new int[counter];
+        System.arraycopy(arr1, 0, arr2, 0, counter);
+        return arr2;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+       // System.out.println(Arrays.toString(solution.intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2})));
+        System.out.println(Arrays.toString(solution.intersect(new int[]{4,9,5}, new int[]{9,4,9,8,4})));
+    }
+}
