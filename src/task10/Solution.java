9120640906d827fcc90d4e100b255da4279d0591
DmIrina <dmytriieva.iryna@gmail.com>
2022-05-06 20:13:51 -0700

    Merge Sorted Array
    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
    
    Merge nums1 and nums2 into a single array sorted in non-decreasing order.
    
    The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.

---
+++ src/task10/Solution.java
@@ -0,0 +1,29 @@
+package task10;
+
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int pointer2 = nums2.length - n;
+        for (int i = 0; i < nums1.length; i++) {
+            if (pointer2 < nums2.length) {
+                if (nums1[i] > nums2[pointer2] || (i > m - 1 && nums1[i] == 0)) {
+                    int temp = nums1[i];
+                    nums1[i] = nums2[pointer2];
+                    nums2[pointer2] = temp;
+                }
+            }
+        }
+        if (n != 0) {
+            if (nums2[pointer2] == 0) {
+                m++;
+                n--;
+            }
+            merge(nums1, m, nums2, n);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+         solution.merge(new int[]{-1,0,0,3,3,3,0,0,0}, 6, new int[]{1,2,2}, 3);
+        // solution.merge(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3);
+    }
+}
